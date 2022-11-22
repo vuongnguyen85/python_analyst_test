6692a4814fdda169f880c6ef6bd05b15bfcfdd1b
Vuong Nguyen <vuong.nguyen@adthena.com>
2022-11-22 09:31:43 +0000

    add more functions to calculate discount and check quantity count

--- main/PriceBasket.py
+++ main/PriceBasket.py
@@ -14,53 +14,83 @@ breadCount = 0
 def getPrice(item):
     return products[item]
 
+def listContainsApples(items):
+    global appleDiscount
+    appleDiscount = False
+    for item in items:
+        if item == 'Apples':
+            appleDiscount = True
+    return appleDiscount
+
+def listContainsAtLeastTwoSoups(items):
+    global soupCount
+    soupCount = 0
+    for item in items:
+        if item == 'Soup':
+            soupCount = soupCount + 1
+    return soupCount >= 2
+
+def listContainsBread(items):
+    global breadCount
+    breadCount = 0
+    for item in items:
+        if item == 'Bread':
+            breadCount = breadCount + 1
+    return breadCount >= 1
+
+def calculateApplesDiscount(items):
+    global appleDiscountValue
+    appleDiscountValue = 0
+    for item in items:
+        if item == 'Apples':
+            appleDiscountValue = appleDiscountValue + (products[item] * .1)
+    return round(appleDiscountValue, 2)
+
+def calculateBreadDiscount(items):
+    global breadDiscountValue
+    breadDiscountValue = 0
+    if listContainsBread(items) and listContainsAtLeastTwoSoups(items):
+        breadDiscountValue = breadDiscountValue + (products["Bread"] * .5)
+    return round(breadDiscountValue, 2)
+
 def calculateBasket(items):
     subtotal = 0
     for item in items:
-        if item == 'Apples':
-            global appleDiscountValue
-            global appleDiscount
-            appleDiscount = True
-            appleDiscountValue = products[item] * .1
-        elif item == 'Soup':
-            global soupCount
-            soupCount = soupCount + 1
-        elif item == 'Bread':
-            global breadCount
-            global breadDiscountValue
-            breadCount = breadCount + 1
-            breadDiscountValue = products[item] * .5
         subtotal = subtotal + products[item]
-    return subtotal
+    return round(subtotal, 2)
 
-def formatPrice(price):
-    return '£{:,.2f}'.format(price)
+def formatPriceWithCurrencySymbol(price):
+    return '£{:,.2f}'.format(price) # ':,' adds comma for thousand values and '.2f' rounds to 2dp
 
 def priceBasket(item1, item2, item3):
     items = [item1, item2, item3]
     subtotal = calculateBasket(items)
-    formattedSubtotal = formatPrice(subtotal) # ':,' adds comma for thousand values and '.2f' rounds to 2dp
-    if appleDiscount or soupCount >= 2:
-        if appleDiscount:
-            print(f'Subtotal: {formattedSubtotal}')
-            formattedAppleDiscountValue = formatPrice(appleDiscountValue)
-            print(f'Apples 10% off: {formattedAppleDiscountValue}')
-            newTotal = subtotal - appleDiscountValue
-            formattedNewTotal = formatPrice(newTotal)
-            print(f'Total: {formattedNewTotal}')
-        elif soupCount >= 2 and breadCount >= 1:
-            print(f'Subtotal: {formattedSubtotal}')
-            formattedBreadDiscountValue = formatPrice(breadDiscountValue)
-            print(f'Half price loaf of bread with 2 tins of Soup: {formattedBreadDiscountValue}')
-            newTotal = subtotal - breadDiscountValue
-            formattedNewTotal = formatPrice(newTotal)
-            print(f'Total: {formattedNewTotal}')
-        else:
-            print(f'Subtotal: {formattedSubtotal} (No offers available - No bread in basket for bread offer)')
-            print(f'Total: {formattedSubtotal}')
+    formattedSubtotal = formatPriceWithCurrencySymbol(subtotal)
+    appleDiscountValue = calculateApplesDiscount(items)
+    breadDiscountValue = calculateBreadDiscount(items)
+
+    # check for special offers
+    if listContainsApples(items):
+        print(f'Subtotal: {formattedSubtotal}')
+        formattedAppleDiscountValue = formatPriceWithCurrencySymbol(appleDiscountValue)
+        print(f'Apples 10% off: {formattedAppleDiscountValue}')
+        newTotal = subtotal - appleDiscountValue
+        formattedNewTotal = formatPriceWithCurrencySymbol(newTotal)
+    elif listContainsAtLeastTwoSoups(items) and listContainsBread(items):
+        print(f'Subtotal: {formattedSubtotal}')
+        formattedBreadDiscountValue = formatPriceWithCurrencySymbol(breadDiscountValue)
+        print(f'Half price loaf of bread with 2 tins of Soup: {formattedBreadDiscountValue}')
+        newTotal = subtotal - breadDiscountValue
+        formattedNewTotal = formatPriceWithCurrencySymbol(newTotal)
     else:
         print(f'Subtotal: {formattedSubtotal} (No offers available)')
+
+    # print total, reuse subtotal if no offers
+    try:
+        print(f'Total: {formattedNewTotal}')
+    except:
         print(f'Total: {formattedSubtotal}')
 
-if __name__ == '__main__':
-    priceBasket(sys.argv[1], sys.argv[2], sys.argv[3])
+priceBasket('Bread', 'Soup', 'Soup')
+# if __name__ == '__main__':
+#     priceBasket(sys.argv[1], sys.argv[2], sys.argv[3])
