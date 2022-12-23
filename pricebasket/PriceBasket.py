5342aef7b33b4ff3dfb4ecacbbbeaaadd5f3664d
Vuong Nguyen <vuong.nguyen@adthena.com>
2022-12-23 16:16:57 +0000

    add add_to_basket to add single product at a time, while also incrementing quantity if product already in basket. Update readme and unit tests

--- pricebasket/PriceBasket.py
+++ pricebasket/PriceBasket.py
@@ -4,12 +4,12 @@ from abc import abstractmethod
 
 
 class Product:
-    def __init__(self, product_name, price):
-        self.product_name = product_name
+    def __init__(self, name, price):
+        self.name = name
         self.price = float(price)
 
     def get_name(self):
-        return self.product_name
+        return self.name
 
     def get_price(self):
         return self.price
@@ -17,38 +17,42 @@ class Product:
 
 class Basket:
     def __init__(self, available_products_in_store):
-        self.basket = {}
-        self.available_products_in_store = available_products_in_store
+        self.basket = {}  # basket is a dictionary of Product object with quantity of that product
+        self.available_products_in_store = available_products_in_store  # available_products_in_store is a list of Product objects
 
     def get_basket(self):
         return self.basket
 
-    def add_to_basket(self, products=[]):
-        basket_with_qty = {}
-        # get quantity of each product in basket (also check that they are available in store)
+    def update_basket(self, products):
         for product in products:
-            for item in self.available_products_in_store:
-                if item.get_name() == product:
-                    basket_with_qty.update({item.product_name: int(products.count(product))})
+            self.add_to_basket(product)
 
-        # create dict of Product class with quantity
-        for product, quantity in basket_with_qty.items():
-            for item in self.available_products_in_store:
-                if item.get_name() == product:
-                    self.basket.update({Product(item.get_name(), item.get_price()): quantity})
+    def add_to_basket(self, product):
+        product_not_in_basket = True
+        if len(self.get_basket()) > 0:
+            for basket_product, quantity in self.get_basket().items():
+                if product == basket_product.get_name():  # check if product we are trying to add is in basket
+                    product_not_in_basket = False
+                    new_product_count = {basket_product: quantity + 1}  # if product is in basket, we just update count for existing Product object
+                    self.basket.update(new_product_count)
+        if product_not_in_basket:  # if product is not in basket, we create new Product object for it
+            for available_product in self.available_products_in_store:
+                if available_product.get_name() == product:
+                    self.basket.update({Product(available_product.get_name(), available_product.get_price()): 1})
 
-    def get_product_quantity_in_basket(self, product_name):
-        try:
-            for product, quantity in self.get_basket().items():
-                if product_name == product.get_name():
-                    return quantity
-        except KeyError:
+    def get_product_quantity_within_basket(self, product_name):
+        for product, quantity in self.get_basket().items():
+            if product_name == product.get_name():
+                return quantity
+        else:
             return 0
 
     def get_product_price(self, product_name):
-        for product, quantity in self.get_basket().items():
+        for product in self.available_products_in_store:
             if product_name == product.get_name():
                 return product.get_price()
+        else:
+            return 0
 
     def calculate_basket_subtotal(self):
         subtotal = 0
@@ -63,16 +67,16 @@ class Offer:
         self.offer_product = offer_product
         self.percentage_off = int(percentage_off)
 
-    def calculate_offer_discount(self, basket):
-        return basket.get_product_price(self.offer_product) * self.percentage_off * .01
+    def calculate_single_use_discount(self, customer_basket):
+        return customer_basket.get_product_price(self.offer_product) * self.percentage_off * .01
 
     @abstractmethod
-    def get_promotion_message(self, basket):
+    def get_promotion_message(self, customer_basket):
         # print promotion message base on offer type
         pass
 
     @abstractmethod
-    def calculate_total_offer_discount_for_basket(self, basket):
+    def calculate_total_discount_for_basket(self, customer_basket):
         # calculate the total discount available based on offer and basket
         pass
 
@@ -83,11 +87,11 @@ class PercentageDiscountOffer(Offer):
         super().__init__(offer_product, percentage_off)
         self.offer_product = offer_product
 
-    def get_promotion_message(self, basket):
-        return f'  {self.offer_product} {self.percentage_off}% off: {Util.format_price(self.calculate_total_offer_discount_for_basket(basket))}'
+    def get_promotion_message(self, customer_basket):
+        return f'  {self.offer_product} {self.percentage_off}% off: {Util.format_price(self.calculate_total_discount_for_basket(customer_basket))}'
 
-    def calculate_total_offer_discount_for_basket(self, basket):
-        return round((super().calculate_offer_discount(basket) * basket.get_product_quantity_in_basket(self.offer_product)), 2)
+    def calculate_total_discount_for_basket(self, customer_basket):
+        return round((super().calculate_single_use_discount(customer_basket) * customer_basket.get_product_quantity_within_basket(self.offer_product)), 2)
         # discount_amount can be multiplied by offer_product quantity
 
 
@@ -99,29 +103,29 @@ class BuyXGetYDiscountOffer(Offer):
         self.additional_product_required = additional_product_required
         self.additional_product_quantity_required = int(additional_product_quantity_required)
 
-    def get_promotion_message(self, basket):
+    def get_promotion_message(self, customer_basket):
         return f'  {self.percentage_off}% off {self.offer_product} when buying ' \
-               f'{self.additional_product_quantity_required} {self.additional_product_required}s: {Util.format_price(self.calculate_total_offer_discount_for_basket(basket))}'
+               f'{self.additional_product_quantity_required} {self.additional_product_required}s: {Util.format_price(self.calculate_total_discount_for_basket(customer_basket))}'
 
-    def calculate_total_offer_discount_for_basket(self, basket):
-        additional_product_quantity = basket.get_product_quantity_in_basket(self.additional_product_required)
+    def calculate_total_discount_for_basket(self, customer_basket):
+        additional_product_quantity = customer_basket.get_product_quantity_within_basket(self.additional_product_required)
         if additional_product_quantity >= self.additional_product_quantity_required:
             # get multiple of (required combination of products), in basket
             promo_limit = round(additional_product_quantity / self.additional_product_quantity_required)
-            # discount_amount can be multiplied by minimum value between multiple of (required combination of products) and quantity of offer_product in basket
-            return round((super().calculate_offer_discount(basket) * min(promo_limit, basket.get_product_quantity_in_basket(self.offer_product))), 2)
+            # single_use_discount amount can be multiplied by minimum value between multiple of (required combination of products) and quantity of offer_product in basket
+            return round((super().calculate_single_use_discount(customer_basket) * min(promo_limit, customer_basket.get_product_quantity_within_basket(self.offer_product))), 2)
         else:
             return 0
 
 
 class PriceCalculator:
-    def __init__(self, Basket, list_of_available_offers):
-        self.basket = Basket
+    def __init__(self, customer_basket, list_of_available_offers):
+        self.customer_basket = customer_basket
         self.current_offers = list_of_available_offers
 
     def offers_can_be_applied_to_basket(self):
         for offer in self.current_offers:
-            if offer.calculate_total_offer_discount_for_basket(self.basket) > 0:
+            if offer.calculate_total_discount_for_basket(self.customer_basket) > 0:
                 return True
         else:
             return False
@@ -130,18 +134,19 @@ class PriceCalculator:
         print(f'Subtotal: {Util.format_price(subtotal)}')
         print('Offers applied:')
         for offer in self.current_offers:
-            if offer.calculate_total_offer_discount_for_basket(self.basket) > 0:
-                offer_discount = offer.calculate_total_offer_discount_for_basket(self.basket)
-                print(offer.get_promotion_message(self.basket))
+            if offer.calculate_total_discount_for_basket(self.customer_basket) > 0:
+                offer_discount = offer.calculate_total_discount_for_basket(self.customer_basket)
+                print(offer.get_promotion_message(self.customer_basket))
                 subtotal -= offer_discount
         print(f'Total: {Util.format_price(subtotal)}')
 
-    def calculate_basket_with_no_offers_applied(self, subtotal):
+    @staticmethod
+    def calculate_basket_with_no_offers_applied(subtotal):
         print(f'Subtotal: {Util.format_price(subtotal)} (No offers available)')
         print(f'Total: {Util.format_price(subtotal)}')
 
     def calculate_customer_basket(self):
-        subtotal = self.basket.calculate_basket_subtotal()
+        subtotal = self.customer_basket.calculate_basket_subtotal()
 
         if self.offers_can_be_applied_to_basket():
             self.calculate_basket_with_offers_applied(subtotal)
@@ -174,24 +179,24 @@ class Util:
 
     @staticmethod
     def get_current_offers():
-        current_offers = []
+        current_offers_in_store = []
         for row in Util.read_csv('data/current_offers.csv')[1:]:
             try:
-                current_offers.append(BuyXGetYDiscountOffer(row[0], row[1], row[2], row[3]))
-            except Exception:
-                current_offers.append(PercentageDiscountOffer(row[0], row[1]))
-        return current_offers
+                current_offers_in_store.append(BuyXGetYDiscountOffer(row[0], row[1], row[2], row[3]))
+            except IndexError:
+                current_offers_in_store.append(PercentageDiscountOffer(row[0], row[1]))
+        return current_offers_in_store
 
 
 if __name__ == '__main__':
-    available_products_in_store = Util.get_available_products_in_store()
+    available_products = Util.get_available_products_in_store()
     current_offers = Util.get_current_offers()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("items", type=str, nargs='+')  # nargs='+' here means at least one value required with no limit of how many arguments (products) could be added
     args = parser.parse_args()
 
-    basket = Basket(available_products_in_store)
-    basket.add_to_basket(args.items)
+    basket = Basket(available_products)
+    basket.update_basket(args.items)
 
     PriceCalculator(basket, current_offers).calculate_customer_basket()
--- test/test_PriceBasket.py
+++ test/test_PriceBasket.py
@@ -18,7 +18,7 @@ bread_promo = current_offers[1]
 
 def create_basket(products):
     basket = Basket(available_products_in_store)
-    basket.add_to_basket(products)
+    basket.update_basket(products)
     return basket
 
 
@@ -28,6 +28,7 @@ class TestPriceBasket(unittest.TestCase):
         self.assertEqual('41p', Util.format_price(0.41))
         self.assertEqual('£1.01', Util.format_price(1.01))
         self.assertEqual('£3.00', Util.format_price(3))
+        self.assertEqual('price can\'t be less than 0.', Util.format_price(-3))
 
     def test_get_product_price(self):
         basket = create_basket(['Apples', 'Bread', 'Soup', 'Milk'])
@@ -35,13 +36,14 @@ class TestPriceBasket(unittest.TestCase):
         self.assertEqual(0.8, basket.get_product_price('Bread'))
         self.assertEqual(1.3, basket.get_product_price('Milk'))
         self.assertEqual(0.65, basket.get_product_price('Soup'))
+        self.assertEqual(0, basket.get_product_price('Banana'))
 
     def test_get_item_quantity(self):
         basket = create_basket(['Apples', 'Apples', 'Apples', 'Bread', 'Soup', 'Soup'])
-        self.assertEqual(3, basket.get_product_quantity_in_basket('Apples'))
-        self.assertEqual(2, basket.get_product_quantity_in_basket('Soup'))
-        self.assertEqual(1, basket.get_product_quantity_in_basket('Bread'))
-        self.assertEqual(0, basket.get_product_quantity_in_basket('Milk'))
+        self.assertEqual(3, basket.get_product_quantity_within_basket('Apples'))
+        self.assertEqual(2, basket.get_product_quantity_within_basket('Soup'))
+        self.assertEqual(1, basket.get_product_quantity_within_basket('Bread'))
+        self.assertEqual(0, basket.get_product_quantity_within_basket('Milk'))
 
     def test_get_promotion_message(self):
         basket = create_basket(['Bread', 'Apples', 'Apples', 'Apples', 'Bread', 'Soup', 'Soup', 'Soup', 'Soup'])
@@ -51,10 +53,10 @@ class TestPriceBasket(unittest.TestCase):
     def test_calculate_total_offer_discount_for_basket(self):
         basket = create_basket(['Bread', 'Apples', 'Apples', 'Apples', 'Bread', 'Soup', 'Soup', 'Soup', 'Soup'])
         # promotion_discount = 3 (apple qty) * 0.1 (10% discount on Apples price)
-        self.assertEqual(0.3, apple_promo.calculate_total_offer_discount_for_basket(basket))
+        self.assertEqual(0.3, apple_promo.calculate_total_discount_for_basket(basket))
 
         # promotion_discount = 2 (qualified bundle qty) * 0.4 (50% discount on Bread price)
-        self.assertEqual(0.8, bread_promo.calculate_total_offer_discount_for_basket(basket))
+        self.assertEqual(0.8, bread_promo.calculate_total_discount_for_basket(basket))
 
     def test_update_customer_basket(self):
         basket = create_basket(['Apples', 'Apples', 'Apples'])
@@ -77,9 +79,9 @@ class TestPriceBasket(unittest.TestCase):
 
         basket = create_basket(['Apples', 'Bread', 'Milk'])
         self.assertEqual(3.1, basket.calculate_basket_subtotal())
-        #
-        # basket = create_basket(['Milk', 'Bread', 'Soup'])
-        # self.assertEqual(2.75, basket.calculate_basket_subtotal())
+
+        basket = create_basket(['Milk', 'Bread', 'Soup'])
+        self.assertEqual(2.75, basket.calculate_basket_subtotal())
 
     def test_offers_can_be_applied_to_basket(self):
         # Need 2 soups to have discount on bread
